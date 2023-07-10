39ba989269d29a830a709efd0022bde318d18b6b
Clovis Lechien <clovis.lechien@epita.fr>
2023-07-10 16:25:30 +0200

    feat(navbar): advanced on the navbar

--- nous-tietze/src/app/app.module.ts
+++ nous-tietze/src/app/app.module.ts
@@ -7,18 +7,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarButtonComponent } from './navbar-button/navbar-button.component';
 import {MatExpansionModule} from "@angular/material/expansion";
 import {MatListModule} from "@angular/material/list";
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarButtonComponent
+    NavbarButtonComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatExpansionModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
